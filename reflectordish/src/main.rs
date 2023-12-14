0316245fd33c2a12761daca27bb6d39759ce3fc8
Rao Zvorovski <rao.zvorovski@outlook.com>
2023-12-14 22:51:24 +0200

    clean up code

--- reflectordish/src/main.rs
+++ reflectordish/src/main.rs
@@ -1,8 +1,8 @@
-use std::{fs, hash};
+use std::{fs};
 
 fn perform_shift(input: &[String]) -> Vec<String> {
     let mut result: Vec<String> = Vec::new();
-    let mut support_levels = input[0].chars().map(|c| -1).collect::<Vec<i64>>();
+    let mut support_levels = input[0].chars().map(|_c| -1).collect::<Vec<i64>>();
     for (i, line) in input.iter().enumerate() {
         let mut new_line = String::new();
         for (j, c) in line.chars().enumerate() {
@@ -26,7 +26,7 @@ fn perform_shift(input: &[String]) -> Vec<String> {
                     }
                     
                 },
-                _ => panic!("Unknown character {}", c),
+                _ => panic!("Unknown character {c}"),
             }
         }
         result.push(new_line);
@@ -38,7 +38,7 @@ fn rotate_90(input: &[String]) -> Vec<String> {
     let mut result: Vec<String> = Vec::new();
     for (i, line) in input.iter().enumerate() {
         let mut new_line = String::new();
-        for (j, c) in line.chars().enumerate() {
+        for (j, _c) in line.chars().enumerate() {
             new_line.push(input[input.len() - j - 1].chars().nth(i).unwrap());
         }
         result.push(new_line);
@@ -49,13 +49,13 @@ fn rotate_90(input: &[String]) -> Vec<String> {
 fn calculate_load_level(input: &[String]) -> u64 {
     let mut total_load = 0;
     for (i, line) in input.iter().enumerate() {
-        for (j, c) in line.chars().enumerate() {
+        for (_j, c) in line.chars().enumerate() {
             match c {
                 '#' | '.' => {},
                 'O' => {
                     total_load += input.len() as u64 - i as u64;
                 },
-                _ => panic!("Unknown character {}", c),
+                _ => panic!("Unknown character {c}"),
             }
         }
 
@@ -64,26 +64,7 @@ fn calculate_load_level(input: &[String]) -> u64 {
 }
 
 fn part_one(input: &[&str]) -> u64 {
-    // let mut total_load = 0;
-    // let mut support_levels = input[0].chars().map(|c| -1).collect::<Vec<i64>>();
-    // for (i, line) in input.iter().enumerate() {
-    //     for (j, c) in line.chars().enumerate() {
-    //         match c {
-    //             '#' => {
-    //                 support_levels[j] = i as i64;
-    //             },
-    //             '.' => {},
-    //             'O' => {
-    //                 support_levels[j] += 1;
-    //                 total_load += input.len() as u64 - support_levels[j] as u64;
-    //             },
-    //             _ => panic!("Unknown character {}", c),
-    //         }
-    //     }
-
-    // }
-    // total_load
-    let mut shifted = perform_shift(input.iter().map(|s| s.to_string()).collect::<Vec<String>>().as_slice());
+    let shifted = perform_shift(input.iter().map(|s| (*s).to_string()).collect::<Vec<String>>().as_slice());
     calculate_load_level(&shifted)
 }
 
@@ -101,15 +82,15 @@ fn perform_cycle(input: &[String]) -> Vec<String> {
 
 fn part_two(input: &[&str]) -> u64 {
     let mut hash_map = std::collections::HashMap::new();
-    let mut intermediate_value = input.iter().map(|s| s.to_string()).collect::<Vec<String>>();
+    let mut intermediate_value = input.iter().map(|s| (*s).to_string()).collect::<Vec<String>>();
     hash_map.insert(intermediate_value.join(""), 0);
 
-    for iter in 1..=1000000 {
+    for iter in 1..=1_000_000 {
         intermediate_value = perform_cycle(&intermediate_value);
         let existing = hash_map.insert(intermediate_value.join(""), iter);
         if let Some(previous_iter) = existing {
             let loop_lenght = iter - previous_iter;
-            let remaining_cycles = (1000000000 - iter) % loop_lenght;
+            let remaining_cycles = (1_000_000_000 - iter) % loop_lenght;
             for _ in 0..remaining_cycles {
                 intermediate_value = perform_cycle(&intermediate_value);
             }
