4cbae9b38b83a3934081b28a13b5779b7b1a0feb
Rao Zvorovski <rao.zvorovski@outlook.com>
2023-12-16 15:46:41 +0200

    day 15

--- lenslibrary/src/main.rs
+++ lenslibrary/src/main.rs
@@ -1,4 +1,45 @@
-use std::fs;
+use std::{fs, str::FromStr};
+
+#[derive(Debug)]
+struct Lens {
+    label: String,
+    focal_length: u64,
+}
+
+impl Lens {
+    fn focusing_power(&self, box_nr: usize, slot_ix: usize) -> u64 {
+        self.focal_length * (box_nr as u64 + 1) * (slot_ix as u64 + 1)
+    }
+}
+
+#[derive(Debug)]
+enum Operation {
+    Equals(String, u64),
+    Dash(String),
+}
+
+
+impl FromStr for Operation {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.ends_with("-") {
+            Ok(Operation::Dash(s[..s.len() - 1].to_string()))
+        } else {
+            let (key, value) = s.split_once('=').ok_or(())?;
+            Ok(Operation::Equals(key.to_string(), value.parse().map_err(|_| ())?))
+        }
+    }
+}
+
+impl Operation {
+    fn get_hash(&self) -> u64 {
+        match self {
+            Operation::Equals(key, _) => hash_string(&key),
+            Operation::Dash(key) => hash_string(&key),
+        }
+    }
+}
 
 fn hash_string(input: &str) -> u64 {
     input
@@ -12,9 +53,49 @@ fn part_one(input: &str) -> u64 {
     input.split(',').map(hash_string).sum()
 }
 
+fn part_two(input: &str) -> u64 {
+    let ops: Vec<Operation> = input
+        .split(',')
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let mut boxes: Vec<Vec<Lens>> = vec![];
+    for _ in 0..256 {
+        boxes.push(vec![]);
+    }
+
+    for op in ops {
+        let hash = op.get_hash();
+        let current_box = &mut boxes[hash as usize];
+        match op {
+            Operation::Equals(key, value) => {
+                if let Some(existing) = current_box.iter_mut().find(|lens| lens.label == key) {
+                    existing.focal_length = value;
+                } else {
+                    current_box.push(Lens {
+                        label: key,
+                        focal_length: value,
+                    });
+                }
+            }
+            Operation::Dash(key) => {
+                current_box.retain(|lens| lens.label != key);
+            }
+        }
+    }
+
+    let mut total_power = 0;
+    for (box_nr, lenses) in boxes.iter().enumerate() {
+        for (slot_ix, lens) in lenses.iter().enumerate() {
+            total_power += lens.focusing_power(box_nr, slot_ix);
+        }
+    }
+    total_power
+}
+
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Error reading input.txt");
     println!("Part one: {}", part_one(&input));
+    println!("Part two: {}", part_two(&input));
 }
 
 #[cfg(test)]
@@ -31,5 +112,10 @@ mod tests {
     #[test]
     fn test_part_one() {
         assert_eq!(part_one(EXAMPLE_INPUT), 1320);
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(EXAMPLE_INPUT), 145);
     }
 }
